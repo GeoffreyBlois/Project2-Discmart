1f12e7a1d45732fa8fa3c877247c9ea4a0e922c4
Ay Sebirka <ay914@revature.net>
2022-06-17 11:53:04 -0500

    last a few touch

--- project2_fe_frittte/src/MainNavBar.jsx
+++ project2_fe_frittte/src/MainNavBar.jsx
@@ -1,30 +1,35 @@
 import { Button,ButtonGroup } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import Image from "./disco2.jpg"
-import Logo from "./WalmartlogoResized.jpg";
+import Logo from "./Walmartlogo.jpg";
+import yellowbody from "./yellowbody.png";
 
 export default function MainNavBar(){
     const navigate = useNavigate();
 
     return(
         <nav>
-             <div className="header">
+             <div className="header2">
              <img src={Logo} alt="Logo"></img>
-            <h1>Welcome to FRITTTE</h1>
+            <h8>Welcome to FRITTTE</h8>
             
-            </div>
+            
+            
 
             <div className="headerPic">
             <img src={Image}></img>
             </div>
             <div className="mainBar">
+            </div>
              <center>
-           
+           <body className="body4">
             <Button style={{borderRadius: 35, backgroundColor: "#0D7AB2", padding: "18px 36px",fontSize: "18px" }} variant="contained" sx={{color:'#FDBB2F'}} onClick={() => navigate("/login")}>Login</Button>
             <Button style={{borderRadius: 35, backgroundColor: "#0D7AB2", padding: "18px 36px",fontSize: "18px" }} variant="contained" sx={{color:'#FDBB2F'}}onClick={() => navigate("/register")}>Sign Up</Button>
             <Button style={{borderRadius: 35, backgroundColor: "#0D7AB2", padding: "18px 36px",fontSize: "18px" }} variant="contained" sx={{color:'#FDBB2F'}} onClick={() => navigate("/Food-Items")}>View all Food Items</Button>
-
+            <img src={yellowbody}></img>
+            </body>
             </center>
+            
             </div>
            
             
--- project2_fe_frittte/src/components/admin/update-product.js
+++ project2_fe_frittte/src/components/admin/update-product.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { useRef, useState } from "react"
 import { Button } from "@mui/material";
-import Logo from "../../WalmartlogoResized.jpg";
+import Logo from "../../Walmartlogo.jpg";
     
 export default function UpdateProduct() {
     
