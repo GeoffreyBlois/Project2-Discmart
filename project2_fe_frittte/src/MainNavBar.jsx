6877a81a1a987a06694f60efcf23ccea6b17c795
Geoffrey <bloisgeoffrey@yahoo.com>
2022-06-17 11:03:17 -0500

    Finalized the UI on the Front End

--- project2_fe_frittte/src/MainNavBar.jsx
+++ project2_fe_frittte/src/MainNavBar.jsx
@@ -2,7 +2,6 @@ import { Button,ButtonGroup } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import Image from "./disco2.jpg"
 import Logo from "./Walmartlogo.jpg";
-import yellowbody from "./yellowbody.png";
 
 export default function MainNavBar(){
     const navigate = useNavigate();
@@ -19,19 +18,15 @@ export default function MainNavBar(){
             <img src={Image}></img>
             </div>
             <div className="mainBar">
-            </div>
+            
              <center>
-           <body className="body4">
+            
             <Button style={{borderRadius: 35, backgroundColor: "#0D7AB2", padding: "18px 36px",fontSize: "18px" }} variant="contained" sx={{color:'#FDBB2F'}} onClick={() => navigate("/login")}>Login</Button>
             <Button style={{borderRadius: 35, backgroundColor: "#0D7AB2", padding: "18px 36px",fontSize: "18px" }} variant="contained" sx={{color:'#FDBB2F'}}onClick={() => navigate("/register")}>Sign Up</Button>
             <Button style={{borderRadius: 35, backgroundColor: "#0D7AB2", padding: "18px 36px",fontSize: "18px" }} variant="contained" sx={{color:'#FDBB2F'}} onClick={() => navigate("/Food-Items")}>View all Food Items</Button>
-            <img src={yellowbody}></img>
-            </body>
-            </center>
             
+            </center>  
             </div>
-           
-            
         </nav>
     )
 }
